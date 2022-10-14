5cc5acb159260e76808e3cefdb305d9b171273fc
Rohit Sanwariya <rohitsanwariya1995@gmail.com>
2022-10-14 14:23:49 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import {TreeNode} from 'primeng/api';
+import { jsPDF } from 'jspdf';
+// import * as $ from 'jquery';
+import * as html2canvas from 'html2canvas';
+declare const $: any;
 
+declare global {
+  interface JQuery {
+    (selector: string): JQuery;
+    orgchart(): JQuery;
+  }
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'primeng-organizationchart-demo-2';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('chartContainer',{static:false}) chartContainer!:ElementRef;
+  constructor() {
+
+  }
+  ngAfterViewInit(): void {
+
+  }
+
+  show(){
+    var datascource = {
+      name: "Lao Lao",
+      title: "general manager",
+      children: [
+        { name: "Bo Miao", title: "department manager" },
+        {
+          name: "Su Miao",
+          title: "department manager",
+          children: [
+            { name: "Tie Hua", title: "senior engineer" },
+            {
+              name: "Hei Hei",
+              title: "senior engineer",
+              children: [
+                { name: "Pang Pang", title: "engineer" },
+                { name: "Xiang Xiang", title: "UE engineer" }
+              ]
+            }
+          ]
+        },
+        { name: "Hong Miao", title: "department manager" },
+        { name: "Chun Miao", title: "department manager" },
+
+        { name: "Hong Miao", title: "department manager" },
+        { name: "Chun Miao", title: "department manager" },
+        { name: "Hong Miao", title: "department manager" },
+        { name: "Chun Miao", title: "department manager" },
+        { name: "Hong Miao", title: "department manager" },
+        { name: "Chun Miao", title: "department manager" },
+        { name: "Hong Miao", title: "department manager" },
+        { name: "Chun Miao", title: "department manager" },
+        { name: "Hong Miao", title: "department manager" },
+        { name: "Chun Miao", title: "department manager" },
+        { name: "Hong Miao", title: "department manager" },
+        { name: "Chun Miao", title: "department manager" },
+        { name: "Hong Miao", title: "department manager" },
+        { name: "Chun Miao", title: "department manager" }
+      ]
+    };
+    ( <any> $(this.chartContainer.nativeElement)).orgchart({
+      data:datascource,
+      visibleLevel: 2,
+      nodeContent: "title",
+      exportButton: true,
+      exportFilename: "MyOrgChart",
+      exportFileextension: "pdf"
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {OrganizationChartModule} from 'primeng/organizationchart';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OrgchartModule } from "@dabeng/ng-orgchart";
+import { NgxOrgChartModule } from 'ngx-org-chart';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    OrganizationChartModule,
+    OrgchartModule,
+    NgxOrgChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
